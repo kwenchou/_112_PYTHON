48d0a10cf6aa90466edf6a6c1b315b415a8b60d3
徐國堂 <roberthsu2003@gmail.com>
2023-01-12 21:15:12 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,4 +1,8 @@
-print(5.0 + 8)
-print(5.0 + 8 + 10)
-print(5.0 + 8 * 2)
-print(2 * (1+2) ** 2 - 2 ** 2 * 2)
+print("abc"*3)
+print("abc" + "xyz")
+# "abc" + 123 -> 錯誤,字串無法和數值運算
+print("abc" + str(123))
+print(str(123))
+print(str(123.4))
+print(int("123") + 456)
+print(float("10.3") + 9)
