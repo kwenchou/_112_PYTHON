f84c938d1850f1f13bb6614d737445d4dcc64217
徐國堂 <roberthsu2003@gmail.com>
2023-01-12 21:33:11 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,8 +1,7 @@
-print("abc"*3)
-print("abc" + "xyz")
-# "abc" + 123 -> 錯誤,字串無法和數值運算
-print("abc" + str(123))
-print(str(123))
-print(str(123.4))
-print(int("123") + 456)
-print(float("10.3") + 9)
+#使用=建立變數
+n = 5 + 8
+m = 7 * 5
+
+#字串的運算
+print("5+8=" + str(n))
+print("7*5=" + str(m))
