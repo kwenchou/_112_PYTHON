06005ff64cb87ec60dd914b4069155d621ff474b
徐國堂 <roberthsu2003@gmail.com>
2023-01-12 20:43:59 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,8 +1,4 @@
-#整數運算
-print(5 + 8)
-print(90 - 10)
-print(4 * 7)
-print(6 / 2)
-print(7 // 2)
-print(7 % 3)
-print(999 ** 200)
+print(5.0 + 8)
+print(5.0 + 8 + 10)
+print(5.0 + 8 * 2)
+print(2 * (1+2) ** 2 - 2 ** 2 * 2)
