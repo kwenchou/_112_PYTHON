cc97347bee49996b980978a0ec593dc7f7feb6e1
徐國堂 <roberthsu2003@gmail.com>
2023-01-17 21:56:50 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,9 +1,17 @@
-score = int(input("請輸入學生分數:"))
-if score >= 60:
-  print("這位學生")
-  print("及格")
-else:
-  print("不及格")
+#90(含)~100分為「優」
+#• 80(含)~89分為「甲」
+#• 70(含)~79分為「乙」
+#• 60(含)~69分為「丙」
+#• 0(含)~59分為「丁」
 
-print("程式結束")
-  
+score = int(input("請輸入學生分數:"))
+if score >= 90:
+  print('優')
+elif score >= 80:
+  print('甲')
+elif score >= 70:
+  print('乙')
+elif score >= 60:
+  print('丙')
+else:
+  print('丁')
