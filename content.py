1ee10064e2fecf44f3ae7ee34d34718d27955c49
徐國堂 <roberthsu2003@gmail.com>
2023-01-17 21:14:49 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,7 +1,8 @@
-#讓使用者輸入梯形的上底、下底及高，程式會計算梯形的面積(上底加下底乘以高除以2)
+x = 10
+x = x + 5
+x
 
-top = eval(input('請輸入梯形的上底(公分):'))
-bottom = eval(input('請輸入梯形的下底(公分):'))
-height = eval(input('請輸入梯形的高(公分):'))
-area = (top + bottom) * height / 2
-print(f"梯形的面積:{area:.2f}平方公分")
+
+x = 10
+x += 5 #複合指定運算子
+x
