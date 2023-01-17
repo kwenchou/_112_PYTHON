66ce4e8dcc2d626635cc390e79ccd6ff192792ed
徐國堂 <roberthsu2003@gmail.com>
2023-01-17 20:36:55 +0800

    Update content.py

--- content.py
+++ content.py
@@ -1,5 +1,5 @@
-chinese = int(input('請輸入國文成績:'))
-english = int(input('請輸入英文成績:'))
+chinese = eval(input('請輸入國文成績:'))
+english = eval(input('請輸入英文成績:'))
 
 #print("國文成績:" + str(chinese) + ",英文成績" + str(english)+",總成績:" + str(chinese+english))
 #字串插補
