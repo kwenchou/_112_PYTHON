18c5fa9a28249c91d77bef990e96f48292315125
徐國堂 <roberthsu2003@gmail.com>
2023-03-09 11:54:46 +0800

    modify

--- 上課用目錄/學院_11202python應用實戰班/lesson11_3.py
+++ 上課用目錄/學院_11202python應用實戰班/lesson11_3.py
@@ -1,9 +1,9 @@
 #請輸入本期大樂透電腦選號的組數:10
-import lottery
+import tools
 
 
 for n in range(int(input("請輸入本期大樂透電腦選號的組數:"))):
-    lottery.lottery_poll()
-    print(f"您將中大獎的大樂透電腦選號第{n+1}組:{lottery.lottery_poll()[:6]}&特別號為:{lottery.lottery_poll()[-1:]}")
+    values = tools.lottery_poll()
+    print(f"您將中大獎的大樂透電腦選號第{n+1}組:{values[:6]}&特別號為:{values[-1:]}")
 print()
 print("!!!!!!!!!!!!!!!祝您中大獎!!!!!!!!!!!!!!!")
