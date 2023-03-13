1f927ecfeaa024c02adc8d1f67c8ca2c4d9ce563
徐國堂 <roberthsu2003@gmail.com>
2023-03-13 10:48:16 +0800

    modify

--- 上課用目錄/學院_11202python應用實戰班/lesson13_1.py
+++ 上課用目錄/學院_11202python應用實戰班/lesson13_1.py
@@ -2,3 +2,4 @@ from lesson13_tools import Person
 
 p1 = Person()
 print(p1)
+print(p1.name)
--- 上課用目錄/學院_11202python應用實戰班/lesson13_tools.py
+++ 上課用目錄/學院_11202python應用實戰班/lesson13_tools.py
@@ -1,4 +1,9 @@
 class Person():
+    def __init__(self) -> None:
+        super().__init__()
+        self.name = "徐國堂"
+
     #override(覆寫)父類別的method
     def __repr__(self) -> str:
-        return "我是Person類別建立出來的實體"
+        own = super().__repr__()
+        return f"我是Person類別建立出來的實體\n{own}"
