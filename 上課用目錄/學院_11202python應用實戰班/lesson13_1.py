69053ef8d299660da7673a927f8e44b9a7a8b6c1
徐國堂 <roberthsu2003@gmail.com>
2023-03-13 11:41:53 +0800

    modify

--- 上課用目錄/學院_11202python應用實戰班/lesson13_1.py
+++ 上課用目錄/學院_11202python應用實戰班/lesson13_1.py
@@ -1,10 +1,11 @@
 from lesson13_tools import Person
 
 p1 = Person("徐國堂",35,True)
+p1.name = "笨蛋"
 print(p1)
 print(p1.name)
 print(p1.age)
-print(p1.isMan)
+print(p1.sex)
 
 
 print("=============")
@@ -13,4 +14,4 @@ p2 = Person("張xx",55,False)
 print(p2)
 print(p2.name)
 print(p2.age)
-print(p2.isMan)
+print(p2.sex)
--- 上課用目錄/學院_11202python應用實戰班/lesson13_tools.py
+++ 上課用目錄/學院_11202python應用實戰班/lesson13_tools.py
@@ -1,9 +1,24 @@
 class Person():
     def __init__(self,name,age,isMan) -> None:
         super().__init__()
-        self.name = name
-        self.age = age
-        self.isMan = isMan
+        self.__name__ = name
+        self.__age__ = age
+        self.__isMan__ = isMan
+    
+    @property
+    def name(self):
+        return self.__name__
+    
+    @property
+    def age(self):
+        return self.__age__
+    
+    @property
+    def sex(self):
+        if self.__isMan__:
+            return "男"
+        else:
+            return "女"
 
     #override(覆寫)父類別的method
     def __repr__(self) -> str:
