8d394b3dd7b1abc9f2ece689a6e0c9b1d0c7a961
徐國堂 <roberthsu2003@gmail.com>
2023-03-13 14:32:39 +0800

    modify

--- 上課用目錄/學院_11202python應用實戰班/lesson14_1/index.py
+++ 上課用目錄/學院_11202python應用實戰班/lesson14_1/index.py
@@ -1,4 +1,11 @@
-import tools
+from tools import Person
+
+def main():
+    p1 = Person("robert")
+    print(p1.name)
+    print(p1.age)
+    print(p1.sex)
+
 
 if __name__ == "__main__":
-    print("我是專案第一個被執行的程式")
+    main()
--- 上課用目錄/學院_11202python應用實戰班/lesson14_1/tools.py
+++ 上課用目錄/學院_11202python應用實戰班/lesson14_1/tools.py
@@ -1,1 +1,20 @@
-print(f"我是{__name__} module,我被執行過一次了")
+class Person():
+    def __init__(self,name,age=20,isMan=True) -> None:
+        self.__name  =  name
+        self.__age = age
+        self.__isMan = isMan
+    
+    @property
+    def name(self):
+        return self.__name
+    
+    @property
+    def age(self):
+        return self.__age
+    
+    @property
+    def sex(self):
+        if self.__isMan:
+            return "男"
+        else:
+            return "女"
