b07b8734f9797dff9f363cd137f69046de31f8fd
徐國堂 <roberthsu2003@gmail.com>
2023-03-14 10:54:15 +0800

    modify

--- 上課用目錄/學院_11202python應用實戰班/lesson15_1/index.py
+++ 上課用目錄/學院_11202python應用實戰班/lesson15_1/index.py
@@ -1,9 +1,20 @@
 from tools import add
 from tools import Tool
+from tools import ONE,TWO,THREE,FOUR,FIVE
 
 def main() -> None:
     print(add(4.5, 9.7))
     print(Tool.add(4.5, 9.7))
+    print(ONE)
+    print(TWO)
+    print(THREE)
+    print(FOUR)
+    print(FIVE)
+    print(Tool.ONE)
+    print(Tool.TWO)
+    print(Tool.THREE)
+    print(Tool.FOUR)
+    print(Tool.FIVE)
 
 if __name__ == '__main__':
     main()
--- 上課用目錄/學院_11202python應用實戰班/lesson15_1/tools.py
+++ 上課用目錄/學院_11202python應用實戰班/lesson15_1/tools.py
@@ -1,7 +1,18 @@
+ONE = 1
+TWO = 2
+THREE  = 3
+FOUR = 4
+FIVE = 5
+
 def add(a,b) -> float:
     return a + b
 
 class Tool(object):
+    ONE = 1
+    TWO = 2
+    THREE  = 3
+    FOUR = 4
+    FIVE = 5
     @staticmethod
     def add(a,b) -> float:
         return a+b
