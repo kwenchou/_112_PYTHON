8f6bea07aec29ae04b2dcb8c76b964ac835dab9d
徐國堂 <roberthsu2003@gmail.com>
2023-03-14 15:42:48 +0800

    modify

--- 上課用目錄/學院_11202python應用實戰班/lesson16/main.py
+++ 上課用目錄/學院_11202python應用實戰班/lesson16/main.py
@@ -1,5 +1,8 @@
+import requests
+
 def main():
-    print("下載aqi資料")
+    response=requests.get('https://data.epa.gov.tw/api/v2/aqx_p_432?api_key=xxxxxxxxxx&limit=1000&sort=ImportDate desc&format=CSV')
+    print(response.text)
 
 if __name__ == "__main__":
     main()
