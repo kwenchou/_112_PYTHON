43ea93cf504686ce9a476ce7e4168c124a1d6218
徐國堂 <roberthsu2003@gmail.com>
2023-03-17 11:18:23 +0800

    modify

--- 上課用目錄/學院_11202python應用實戰班/lesson17/tools.py
+++ 上課用目錄/學院_11202python應用實戰班/lesson17/tools.py
@@ -2,6 +2,20 @@ import requests
 import csv
 from io import StringIO
 
+class Site(object):
+    def __init__(self,name,county,aqi):
+        super().__init__()
+        self.site_name = name
+        self.county = county
+        try:
+            self.aqi = int(aqi)
+        except:
+            self.aqi = 999
+
+    def __repr__(self):
+        return f"站點:{self.site_name},城市:{self.county},aqi:{self.aqi}"
+
+
 class Taiwan_AQI():    
     API_KEY = "b8416fe0-3673-4eac-be88-7ac4bb9fce06"
     @classmethod
@@ -17,12 +31,12 @@ class Taiwan_AQI():
             file = StringIO(response.text,newline='')
             csvReader = csv.reader(file)
             next(csvReader)
-            
-            return list(csvReader)
-            '''
+            dataList = []
             for item in csvReader:
-                print(item[0])
-            '''
+                site = Site(item[0],item[1],item[2])
+                dataList.append(site)
+            return dataList
+            
 
             
         else:
