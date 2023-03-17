a345681ff524abb1bc8e1dc1db04923241cfc4e1
徐國堂 <roberthsu2003@gmail.com>
2023-03-17 11:59:05 +0800

    modify

--- 上課用目錄/學院_11202python應用實戰班/lesson17/main.py
+++ 上課用目錄/學院_11202python應用實戰班/lesson17/main.py
@@ -1,6 +1,17 @@
 
 from tools import Taiwan_AQI
 
+def get_best(dataList) -> list:
+    sorted_data = sorted(dataList,key=lambda a:a.aqi,reverse=True)
+    for item in sorted_data:
+        print(item)
+
+def get_bad(dataList) -> list:
+    pass
+
+def print_site_level() -> None:
+    pass
+
 def main():
     try:
         aqi_list = Taiwan_AQI.download_aqi()
@@ -8,9 +19,7 @@ def main():
         print(str(err))
         return
     
-    for item in aqi_list:
-        print(item)
-    
+    get_best(aqi_list)
     
 
 if __name__ == "__main__":
