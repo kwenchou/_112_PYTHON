50054e92601a0b5d99fa599958bd37a1d9f595e1
徐國堂 <roberthsu2003@gmail.com>
2023-04-02 16:23:07 +0800

    modify

--- 上課用目錄/2023_02_26_星期日/lesson12_1/main.py
+++ 上課用目錄/2023_02_26_星期日/lesson12_1/main.py
@@ -6,12 +6,17 @@ def main() -> None:
     print(tools.NAME)
     print(tools.get_weather())
     p1 = Person("徐國堂",40)
-    p1.score = 78
-    p1.print_detail()
+    p1.score = 78   
+    print(p1.name)
+    print(p1.age)
+    print(p1.score)
 
     p2 = Person("alice",24)
     p2.score = 65
-    p2.print_detail()
+    print(p2.name)
+    print(p2.age)
+    print(p2.score)
+    
 
 
 if __name__ == "__main__":    
--- 上課用目錄/2023_02_26_星期日/lesson12_1/tools.py
+++ 上課用目錄/2023_02_26_星期日/lesson12_1/tools.py
@@ -8,12 +8,22 @@ def get_weather() -> str:
 class Person:
     def __init__(self,name,age,score=0):
         #實體的attribute,可被取出,也可以被寫入
-        self.name = name
-        self.age = age
+        self.__name = name
+        self.__age = age
         self.score = score
+
+    #實體的property,可以讓實體取出
+    @property
+    def name(self) -> str:
+        return self.__name
+    
+    @property
+    def age(self) -> int:
+        return self.__age
+
 
     #實體方法
     def print_detail(self) -> None:
-        print(f"name:{self.name}")
-        print(f"age:{self.age}")
+        print(f"name:{self.__name}")
+        print(f"age:{self.__age}")
         print(f"score:{self.score}")
