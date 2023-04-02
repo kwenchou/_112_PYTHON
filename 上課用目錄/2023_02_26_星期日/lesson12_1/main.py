094952824e30d2f87cf8914fe5cac32fb27b3682
徐國堂 <roberthsu2003@gmail.com>
2023-04-02 16:01:39 +0800

    modify

--- 上課用目錄/2023_02_26_星期日/lesson12_1/main.py
+++ 上課用目錄/2023_02_26_星期日/lesson12_1/main.py
@@ -6,13 +6,12 @@ def main() -> None:
     print(tools.NAME)
     print(tools.get_weather())
     p1 = Person("徐國堂",40)
-    print(f"p1的name:{p1.name}")
-    print(f"p1的age:{p1.age}")
-    print(f"p1的score:{p1.score}")
+    p1.score = 78
+    p1.print_detail()
+
     p2 = Person("alice",24)
-    print(f"p2的name:{p2.name}")
-    print(f"p2的age:{p2.age}")
-    print(f"p2的score:{p2.score}")
+    p2.score = 65
+    p2.print_detail()
 
 
 if __name__ == "__main__":    
--- 上課用目錄/2023_02_26_星期日/lesson12_1/tools.py
+++ 上課用目錄/2023_02_26_星期日/lesson12_1/tools.py
@@ -7,7 +7,13 @@ def get_weather() -> str:
 
 class Person:
     def __init__(self,name,age,score=0):
-        #實體的attribute
+        #實體的attribute,可被取出,也可以被寫入
         self.name = name
         self.age = age
         self.score = score
+
+    #實體方法
+    def print_detail(self) -> None:
+        print(f"name:{self.name}")
+        print(f"age:{self.age}")
+        print(f"score:{self.score}")
