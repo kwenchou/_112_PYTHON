9b894567c549803e60428db6acd07bd8b7fae9fb
徐國堂 <roberthsu2003@gmail.com>
2023-04-02 14:50:11 +0800

    modify

--- 上課用目錄/2023_02_26_星期日/lesson12_1/main.py
+++ 上課用目錄/2023_02_26_星期日/lesson12_1/main.py
@@ -3,6 +3,7 @@ import tools
 def main() -> None:
     print(tools.PI)
     print(tools.NAME)
+    print(tools.get_weather())
 
 
 if __name__ == "__main__":    
--- 上課用目錄/2023_02_26_星期日/lesson12_1/tools.py
+++ 上課用目錄/2023_02_26_星期日/lesson12_1/tools.py
@@ -1,2 +1,6 @@
+import random
 PI = 3.1415962
 NAME = "我是tools內的常數Name"
+
+def get_weather() -> str:
+    return random.choice(['晴天','陰天','雨天','大太陽'])
