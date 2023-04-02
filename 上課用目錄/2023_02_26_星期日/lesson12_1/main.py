aceda80e70bf49a6c5240ff037c923d77858e36b
徐國堂 <roberthsu2003@gmail.com>
2023-04-02 16:32:26 +0800

    modify

--- 上課用目錄/2023_02_26_星期日/lesson12_1/main.py
+++ 上課用目錄/2023_02_26_星期日/lesson12_1/main.py
@@ -5,14 +5,12 @@ def main() -> None:
     print(tools.PI)
     print(tools.NAME)
     print(tools.get_weather())
-    p1 = Person("徐國堂",40)
-    p1.score = 78   
+    p1 = tools.get_person("徐國堂",40,78)   
     print(p1.name)
     print(p1.age)
     print(p1.score)
 
-    p2 = Person("alice",24)
-    p2.score = 65
+    p2 = tools.get_person("alice",24,65)    
     print(p2.name)
     print(p2.age)
     print(p2.score)
--- 上課用目錄/2023_02_26_星期日/lesson12_1/tools.py
+++ 上課用目錄/2023_02_26_星期日/lesson12_1/tools.py
@@ -5,11 +5,14 @@ NAME = "我是tools內的常數Name"
 def get_weather() -> str:
     return random.choice(['晴天','陰天','雨天','大太陽'])
 
+def get_person(name, age, score):
+    return Person(name,age,score=score)
+
 class Person:
-    def __init__(self,name,age,score=0):
-        #實體的attribute,可被取出,也可以被寫入
+    def __init__(self,name,age,score=0):        
         self.__name = name
         self.__age = age
+        #實體的attribute,可被取出,也可以被寫入
         self.score = score
 
     #實體的property,可以讓實體取出
@@ -22,7 +25,7 @@ class Person:
         return self.__age
 
 
-    #實體方法
+    #實體方法method()
     def print_detail(self) -> None:
         print(f"name:{self.__name}")
         print(f"age:{self.__age}")
