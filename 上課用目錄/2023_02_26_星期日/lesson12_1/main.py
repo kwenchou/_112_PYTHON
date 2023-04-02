78e65c2499ab4ad0ce8b27c46e4c57fc46a9c39e
徐國堂 <roberthsu2003@gmail.com>
2023-04-02 15:27:40 +0800

    modify

--- 上課用目錄/2023_02_26_星期日/lesson12_1/main.py
+++ 上課用目錄/2023_02_26_星期日/lesson12_1/main.py
@@ -1,10 +1,14 @@
 import tools
+from tools import Person
 
 def main() -> None:
     print(tools.PI)
     print(tools.NAME)
     print(tools.get_weather())
-    print(tools.Person)
+    p1 = Person()
+    print(type(p1))
+    p2 = Person()
+    print(type(p2))
 
 
 if __name__ == "__main__":    
